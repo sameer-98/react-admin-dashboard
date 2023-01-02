eb2c578cd52528692215839f73ee8e26ee1e47a2
Sameer <sameer_ahmed141@hotmail.com>
2023-01-02 20:48:40 +0500

    Add Geography Chart

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
 import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
-// import Bar from "./scenes/bar";
-// import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
+import Bar from "./scenes/bar";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
 import Form from "./scenes/form";
 import FAQ from "./scenes/faq";
-// import Geography from "./scenes/geography";
+import Geography from "./scenes/geography";
 import { Routes, Route } from "react-router-dom";
 import Calendar from "./scenes/calendar"
 
@@ -31,11 +31,11 @@ function App() {
                 <Route path="/contacts" element={<Contacts/>}/>
                 <Route path="/invoices" element={<Invoices/>}/>
                 <Route path="/form" element={<Form/>}/>
-                {/* <Route path="/bar" element={<Bar/>}/> */}
-                {/* <Route path="/pie" element={<Pie/>}/> */}
-                {/* <Route path="/line" element={<Line/>}/> */}
+                <Route path="/bar" element={<Bar/>}/>
+                <Route path="/pie" element={<Pie/>}/>
+                <Route path="/line" element={<Line/>}/>
                 <Route path="/faq" element={<FAQ/>}/>
-                {/* <Route path="/geography" element={<Geography/>}/> */}
+                <Route path="/geography" element={<Geography/>}/>
                 <Route path="/calendar" element={<Calendar/>}/>
             </Routes>
           </main>
